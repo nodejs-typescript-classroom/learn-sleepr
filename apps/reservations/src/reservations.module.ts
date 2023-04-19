cf397bff4c7476a0a7ffd8e71955d4957869327c
gson Liang <yuanyu90221@gmail.com>
2023-04-19 23:39:23 +0800

    feat(validation-and-logger): validation-and-logger
    add validation and logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/shared';
+import { DatabaseModule, LoggerModule } from '@app/shared';
 import { ReservationsRepository } from './reservations.repository';
 import {
   ReservationDocument,
@@ -14,6 +14,7 @@ import {
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
