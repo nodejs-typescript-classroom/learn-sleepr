f679f13df9941acd1554231f6b0bdc7a3dc7d312
gson Liang <yuanyu90221@gmail.com>
2023-05-24 00:24:42 +0800

    feat(e2e-health-check): e2e-health-check
    e2e-health-check

---
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -0,0 +1,39 @@
+import { ping } from 'tcp-ping';
+describe('Health', () => {
+  test('Reservations', async () => {
+    const response = await fetch('http://reservations:3000');
+    expect(response.ok).toBeTruthy();
+  });
+  test('Auth', async () => {
+    const response = await fetch('http://auth:3001');
+    expect(response.ok).toBeTruthy();
+  });
+  test('Payments', (done) => {
+    ping(
+      {
+        address: 'payments',
+        port: 3003,
+      },
+      (error) => {
+        if (error) {
+          fail();
+        }
+        done();
+      },
+    );
+  });
+  test('Notifications', (done) => {
+    ping(
+      {
+        address: 'notifications',
+        port: 3004,
+      },
+      (error) => {
+        if (error) {
+          fail();
+        }
+        done();
+      },
+    );
+  });
+});
